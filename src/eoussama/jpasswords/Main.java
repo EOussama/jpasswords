a23f2ab6d69d2d51490db0add217185a9ad7384b
Eoussama <oussama_gtacj@live.fr>
2018-06-09 00:15:48 +0000

    Password hashing fixed

--- src/eoussama/jpasswords/Main.java
+++ src/eoussama/jpasswords/Main.java
@@ -6,7 +6,7 @@ public class Main {
         String password, salt, result;
 
         password = "password123";
-        salt = "!OvpytBUjdE56CApLG0c %CU0HwAzQiKn1*LGX6hW1ntwwVqBW@m*kjOdMXgLGBilcxh55WhdLd mhwvdhoC9X%4DxKAbgvvWyqaCPzMRfaH44H$ty*vyzV&^pLHa!NP";//jPasswords.Generate(128, null);
+        salt = "fpogjp,";//jPasswords.Generate(128, null);
         result = jPasswords.Hash(password, salt, -1);
 
         System.out.println("Password: \t" + password);
--- src/eoussama/jpasswords/jPasswords.java
+++ src/eoussama/jpasswords/jPasswords.java
@@ -35,7 +35,8 @@ public abstract class jPasswords {
         while(count++ < __hashedString.length() - 1) {
             char __character = (char)((int)__hashedString.charAt(count) + __hashedString.length());
             Random rand = new Random();
-            __character = __character > 126 ? (char)(rand.nextInt(126 - 32 + 1) + 32) : __character < 32 ? (char)(rand.nextInt(126 - 32 + 1) + 32) : __character;
+            //__character = __character > 126 ? (char)(rand.nextInt(126 - 32 + 1) + 32) : __character < 32 ? (char)(rand.nextInt(126 - 32 + 1) + 32) : __character;
+            __character = __character > 126 || __character < 32 ? (char)jPasswords.OverWrap((int)__character, 126, 32) : __character;
             __hashedString.setCharAt(count, __character);
         }
 
@@ -44,5 +45,13 @@ public abstract class jPasswords {
 
     public static boolean Authenticate(String password, String salt, String target) {
         return jPasswords.Hash(password, salt, -1) == target;
+    }
+
+    private static int OverWrap(int num, int max, int min) {
+        int output = min;
+
+        while(num-- > 0) output = output > max ? min : output + 1;
+
+        return output;
     }
 }
