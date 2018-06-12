9a8e9980af59f8a839f4f6527d4d43bbfc49c5dd
Eoussama <oussama_gtacj@live.fr>
2018-06-12 00:08:12 +0000

    Quotation marks escaped

--- src/eoussama/jpasswords/Main.java
+++ src/eoussama/jpasswords/Main.java
@@ -3,11 +3,6 @@ package eoussama.jpasswords;
 public class Main {
 
     public static void main(String[] args) {
-        String password = "123456789";
-        String salt = jPasswords.Generate(20,null);
-        String hashed_and_salted_password = jPasswords.Hash(password, salt, -1);
-
-        System.out.println("Auth: " + jPasswords.Authenticate("123456789", salt, hashed_and_salted_password));
-        System.out.println("Auth: " + jPasswords.Authenticate("123456789", null, hashed_and_salted_password));
+        System.out.println(jPasswords.Hash("teamdrd", jPasswords.Generate(128, new String[] { jPasswords.NUMBERS, jPasswords.LOWER_CASE }), 10));
     }
 }
